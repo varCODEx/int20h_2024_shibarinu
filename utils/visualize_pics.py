94f32e83c88c1cb0716585f773bdc0531dad42d9
Borys Kovba <borys.kovba@gmail.com>
2024-03-02 21:02:54 +0200

    fix merge detected with masked

--- utils/visualize_pics.py
+++ utils/visualize_pics.py
@@ -1,8 +1,13 @@
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
+from numpy import random
 
-def visualize_pics(ims, n=480):
+
+def visualize_pics(ims, n=480, shuffle=False):
+    if shuffle:
+        random.shuffle(ims)
+
     n = n // 3 * 3
 
     plt.figure(figsize=(10, n // 3 * 2))
